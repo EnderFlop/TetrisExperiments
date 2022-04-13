cdce0a45cac7c044fd13b8af62aa6c1f2600d329
EnderFlop <enderflop@gmail.com>
2022-04-12 20:38:15 -0500

    board refactor complete
    The goal was to make the board isolated, so you can make a copy of the board and mess around with it without affecting the real game. This will be used to get statistics about possible piece placements the AI could make.

--- app.js
+++ app.js
@@ -1,294 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => { //Fires when html is fully loaded
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
   const startBtn = document.querySelector('#start-button')
-  const heightText = document.querySelector('#height')
-  const bumpinessText = document.querySelector('#bumpiness')
-  const holesText = document.querySelector('#holes')
   const height = 20
   const width = 10
-  let nextRandom = 0
   let gameStarted = false
   let timerId
-  let score = 0
 
-  //The Tetrominoes
-  const lLeftTetromino = [
-    //4 rotations, each in an array.
-    //Start at Initial Spawn, then rotate right.
-    [0, 1, 2, width + 2],
-    [1, width + 1, width * 2, width * 2 + 1],
-    [width, width * 2, width * 2 + 1, width * 2 + 2],
-    [1, 2, width + 1, width * 2 + 1]
-  ]
-  const lRightTetromino = [
-    [0, 1, 2, width],
-    [0, 1, width + 1, width * 2 + 1],
-    [width + 2, width * 2, width * 2 + 1, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2 + 2]
-  ]
-  const zLeftTetromino = [
-    [0, 1, width + 1, width + 2],
-    [2, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width * 2 + 1, width * 2 + 2],
-    [1, width, width + 1, width * 2]
-  ]
-  const zRightTetromino = [
-    [1, 2, width, width + 1],
-    [1, width + 1, width + 2, width * 2 + 2],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1]
-  ]
-  const sTetromino = [
-    [0, 1, 2, 3],
-    [2, width + 2, width * 2 + 2, width * 3 + 2],
-    [0, 1, 2, 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1]
-  ]
-  const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
-  ]
-  const tTetromino = [
-    [0, 1, 2, width + 1],
-    [1, width, width + 1, width * 2 + 1],
-    [width + 1, width * 2, width * 2 + 1, width * 2 + 2],
-    [1, width + 1, width + 2, width * 2 + 1]
-  ]
-
-  const theTetrominos = [lLeftTetromino, lRightTetromino, zLeftTetromino, zRightTetromino, sTetromino, oTetromino, tTetromino]
-  const colors = ['orange', 'blue', 'red', 'green', 'cyan', 'yellow', 'purple']
-
-  let currentPosition = 4
-  let currentRotation = 0
-
-  let random = Math.floor(Math.random() * theTetrominos.length)
-  let current = theTetrominos[random][currentRotation]
-
-  //draw the Tetromino
-  function draw() {
-    //for every coordinate in a rotation, add the 'tetromino' class to the div at that position.
-    current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino')
-      squares[currentPosition + index].style.backgroundColor = colors[random]
-    })
-  }
-
-  function undraw(){
-    current.forEach(index => {
-      squares[currentPosition + index].classList.remove('tetromino')
-      squares[currentPosition + index].style.backgroundColor = ''
-    })
-  }
-
-  //stop the tetromino
-  function freeze() {
-    //if the row below the tetromino contains a 'taken' div, change the divs where the tetromino lies to also be taken.
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-      //start a new tetromino falling
-      random = nextRandom
-      nextRandom = Math.floor(Math.random() * theTetrominos.length)
-      current = theTetrominos[random][currentRotation]
-      currentPosition = 4
-      draw()
-      displayShape()
-      addScore()
-      gameOver()
-      statsHelper()
-    }
-  }
+  let board = new Board(width, height, grid, squares)
 
   //assign functions to keyCodes
   function control(e) {
-    if (e.keyCode === 37) moveLeft()
-    else if (e.keyCode === 38) rotate()
-    else if (e.keyCode === 39) moveRight()
-    else if (e.keyCode === 40) moveDown()
-    else if (e.keyCode === 32) forceDown()
-  }
-
-  //move the tetromino down
-  function moveDown() {
-    undraw()
-    currentPosition += width
-    draw()
-    freeze()
-  }
-
-  function forceDown() {
-    while (!current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-      undraw()
-      currentPosition += width
-      draw()
-    }
-    freeze()
-  }
-
-  function rotate(){
-    undraw()
-    pastRotation = currentRotation
-    currentRotation = (currentRotation + 1) % 4
-    current = theTetrominos[random][currentRotation]
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      console.log('cant rotate that!')
-      //current = theTetrominos[random][pastRotation]
-      currentPosition -= width
-    }
-    draw()
-    freeze()
-  }
-
-  //move the tetrominos left, unless it is at the edge or there is a blockage.
-  function moveLeft(){
-    undraw()
-    //if any of the indexes is touching the left edge (10 % width=(10) === 0)
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
-    //if okay, move it left
-    if (!isAtLeftEdge) currentPosition -= 1
-    //if colliding with any taken divs, move it back.
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) currentPosition += 1
-    draw()
-  }
-
-  //move tetrominos right, same logic as moveLeft
-  function moveRight(){
-    undraw()
-    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
-    if (!isAtRightEdge) currentPosition += 1
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) currentPosition -= 1
-    draw()
-  }
-
-  //show up-next tetromino in mini-grid display
-  const displaySquares = document.querySelectorAll('.mini-grid div')
-  const miniGridWidth = 4
-  const displayIndex = 0
-  
-  //all tetromino's first rotations
-  const upNextTetrominoes = [
-    [0, 1, 2, miniGridWidth + 2], //lLeftTetromino
-    [0, 1, 2, miniGridWidth], //lRigthTetromino
-    [0, 1, miniGridWidth + 1, miniGridWidth + 2], //zLeftTetromino
-    [1, 2, miniGridWidth, miniGridWidth + 1], //zRightTetromino
-    [0, 1, 2, 3], //sTetromino
-    [0, 1, miniGridWidth, miniGridWidth + 1], //oTetromino
-    [0, 1, 2, miniGridWidth + 1], //tTetromino
-  ]
-
-  //display the shape in the mini-grid
-  function displayShape() {
-    //cleanse the whole minigrid
-    displaySquares.forEach(square => {
-      square.classList.remove('tetromino')
-      square.style.backgroundColor = ''
-    })
-    upNextTetrominoes[nextRandom].forEach(index => {
-      displaySquares[displayIndex + index].classList.add('tetromino')
-      displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
-    })
+    if (e.keyCode === 37) board.moveLeft()
+    else if (e.keyCode === 38) board.rotate()
+    else if (e.keyCode === 39) board.moveRight()
+    else if (e.keyCode === 40) board.moveDown()
+    else if (e.keyCode === 32) board.forceDown()
   }
 
   //start and pause functionality
   startBtn.addEventListener('click', () => {
-    if (timerId) {
-      clearInterval(timerId)
+    if (board.timerId) {
+      clearInterval(board.timerId)
       document.removeEventListener('keydown', control)
-      timerId = null
+      board.timerId = null
     } else {
-      draw()
-      timerId = setInterval(moveDown, 500)
+      board.draw()
+      board.startTimer()
       document.addEventListener('keydown', control)
       if (!gameStarted) { //if the game is just now starting, create a random shape. prevents exploit from pausing and unpausing to change shape.
-        nextRandom = Math.floor(Math.random()*theTetrominos.length)
-        displayShape()
+        board.nextRandom = Math.floor(Math.random()*board.theTetrominos.length)
+        board.displayShape()
         gameStarted = true
       }
     }
     startBtn.blur() //removes focus from button to prevent pausing by spacebar (force drop)
   })
-
-  //add score and remove completed rows
-  function addScore() {
-    for (let i=0; i < 199; i += width) {
-      const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
-      if (row.every(index => squares[index].classList.contains('taken'))) {
-        score += 10
-        ScoreDisplay.innerHTML = score
-        row.forEach(index => {
-          squares[index].classList.remove('tetromino')
-          squares[index].classList.remove('taken')
-          squares[index].style.backgroundColor = ''
-        })
-        const squaresRemoved = squares.splice(i, width)
-        squares = squaresRemoved.concat(squares)
-        squares.forEach(cell => grid.appendChild(cell))
-      }
-    }
-  }
-
-  //game over
-  function gameOver(){
-    //if our block is inside any other blocks, the game ends. This happens when a block spawns into another one.
-    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))){
-      ScoreDisplay.innerHTML = 'end'
-      clearInterval(timerId)
-      document.removeEventListener('keydown', control)
-    }
-  }
-
-
-  //AI HELPER FUNCTIONS
-  function getHeight(){
-    for (let i = 0; i < 209; i++){
-      if (squares[i].classList.contains('taken')) {
-        heightText.innerHTML = (height - Math.floor(i / width)) //current level. height - (index found / width)
-        break 
-      }
-    }
-  }
-
-  function getBumpiness(){
-    //find height of first column,  add the abs() difference of height to the next column, continue
-    totalBumpiness = 0
-    let newColumnHeight = 0
-    let pastColumnHeight = -1
-    for (let column = 0; column < 9; column++){
-      for (let i = column; i < height * width + width; i += width) { //i goes to 209 (height * width + width) in order to account for the '0' height floor
-        if (squares[i].classList.contains('taken')) {
-          newColumnHeight = (height - Math.floor(i / width)) //current height formula
-          if (pastColumnHeight != -1){ //let it run once before doing math
-            totalBumpiness += Math.abs(newColumnHeight - pastColumnHeight)
-          }
-          pastColumnHeight = newColumnHeight
-          break //exit this column
-        }
-      }
-    }
-    bumpinessText.innerHTML = totalBumpiness
-  }
-
-  function getHoles(){
-    let numHoles = 0
-    for (let column = 0; column < 9; column++){
-      let blockHit = false
-      for (let i = column; i < height * width; i += width) {
-        //if there was a block higher up and this is an air pocket, that's a hole.
-        if (blockHit && !squares[i].classList.contains('taken')) numHoles++
-        else if (!blockHit && squares[i].classList.contains('taken')) blockHit = true
-      }
-    }
-    holesText.innerHTML = numHoles
-  }
-
-  function statsHelper(){
-    getHeight()
-    getBumpiness()
-    getHoles()
-  }
 
   //AI analysis function.
   //should copy board and get statistics for every possible placement of piece (every rotation in every column)
--- board.js
+++ board.js
@@ -1,123 +1,211 @@
 class Board {
-  constructor(width, height) {
-    this.grid = [];
-    for (let i = 0; i < height + 1; i++) {
-      for (let j = 0; j < width; j++){
-        this.grid.push(document.createElement('div'))
-      } 
-    }
+  constructor(width, height, parentGrid, squares) {
+
+    //BOARD CONSTANTS
+    this.width = width
+    this.height = height
+    this.grid = squares
+    this.parentGrid = parentGrid
+    this.nextRandom
+
+    //HTML FIELDS
+    this.heightText = document.querySelector('#height')
+    this.bumpinessText = document.querySelector('#bumpiness')
+    this.holesText = document.querySelector('#holes')
+    this.scoreText = document.querySelector('#score')
+    this.displaySquares = document.querySelectorAll('.mini-grid div')
+
+    //STATISTICS
+    this.currentHeight
+    this.currentBump
+    this.currentHoles
+    this.score = 0
+
+    //The Tetrominoes
+    this.lLeftTetromino = [
+      //4 rotations, each in an array.
+      //Start at Initial Spawn, then rotate right.
+      [0, 1, 2, this.width + 2],
+      [1, this.width + 1, this.width * 2, this.width * 2 + 1],
+      [this.width, this.width * 2, this.width * 2 + 1, this.width * 2 + 2],
+      [1, 2, this.width + 1, this.width * 2 + 1]
+    ]
+    this.lRightTetromino = [
+      [0, 1, 2, this.width],
+      [0, 1, this.width + 1, this.width * 2 + 1],
+      [this.width + 2, this.width * 2, this.width * 2 + 1, this.width * 2 + 2],
+      [1, this.width + 1, this.width * 2 + 1, this.width * 2 + 2]
+    ]
+    this.zLeftTetromino = [
+      [0, 1, this.width + 1, this.width + 2],
+      [2, this.width + 1, this.width + 2, this.width * 2 + 1],
+      [this.width, this.width + 1, this.width * 2 + 1, this.width * 2 + 2],
+      [1, this.width, this.width + 1, this.width * 2]
+    ]
+    this.zRightTetromino = [
+      [1, 2, this.width, this.width + 1],
+      [1, this.width + 1, this.width + 2, this.width * 2 + 2],
+      [this.width + 1, this.width + 2, this.width * 2, this.width * 2 + 1],
+      [0, this.width, this.width + 1, this.width * 2 + 1]
+    ]
+    this.sTetromino = [
+      [0, 1, 2, 3],
+      [2, this.width + 2, this.width * 2 + 2, this.width * 3 + 2],
+      [0, 1, 2, 3],
+      [1, this.width + 1, this.width * 2 + 1, this.width * 3 + 1]
+    ]
+    this.oTetromino = [
+      [0, 1, this.width, this.width + 1],
+      [0, 1, this.width, this.width + 1],
+      [0, 1, this.width, this.width + 1],
+      [0, 1, this.width, this.width + 1]
+    ]
+    this.tTetromino = [
+      [0, 1, 2, this.width + 1],
+      [1, this.width, this.width + 1, this.width * 2 + 1],
+      [this.width + 1, this.width * 2, this.width * 2 + 1, this.width * 2 + 2],
+      [1, this.width + 1, this.width + 2, this.width * 2 + 1]
+    ]
+
+    this.theTetrominos = [this.lLeftTetromino, this.lRightTetromino, this.zLeftTetromino, this.zRightTetromino, this.sTetromino, this.oTetromino, this.tTetromino]
+    this.colors = ['orange', 'blue', 'red', 'green', 'cyan', 'yellow', 'purple']
+
+    this.currentRotation = 0
+    this.random = Math.floor(Math.random() * this.theTetrominos.length)
+    this.current = this.theTetrominos[this.random][this.currentRotation]
+
+    this.currentPosition = 4
+
+    //show up-next tetromino in mini-grid display
+    this.displayIndex = 0
+    const miniGridWidth = 4
+    //all tetromino's first rotations
+    this.upNextTetrominoes = [
+      [0, 1, 2, miniGridWidth + 2], //lLeftTetromino
+      [0, 1, 2, miniGridWidth], //lRigthTetromino
+      [0, 1, miniGridWidth + 1, miniGridWidth + 2], //zLeftTetromino
+      [1, 2, miniGridWidth, miniGridWidth + 1], //zRightTetromino
+      [0, 1, 2, 3], //sTetromino
+      [0, 1, miniGridWidth, miniGridWidth + 1], //oTetromino
+      [0, 1, 2, miniGridWidth + 1], //tTetromino
+    ]
   }
 
-  draw(current) {
+  startTimer() {
+    self = this
+    this.timerId = setInterval(function() {self.moveDown()} , 500)
+  }
+
+  draw() {
     //for every coordinate in a rotation, add the 'tetromino' class to the div at that position.
-    current.forEach(index => {
-      this.grid[currentPosition + index].classList.add('tetromino')
-      this.grid[currentPosition + index].style.backgroundColor = colors[random]
+    this.current.forEach(index => {
+      this.grid[this.currentPosition + index].classList.add('tetromino')
+      this.grid[this.currentPosition + index].style.backgroundColor = this.colors[this.random]
     })
   }
 
-  undraw(current){
-    current.forEach(index => {
-      this.grid[currentPosition + index].classList.remove('tetromino')
-      this.grid[currentPosition + index].style.backgroundColor = ''
+  undraw() {
+    this.current.forEach(index => {
+      this.grid[this.currentPosition + index].classList.remove('tetromino')
+      this.grid[this.currentPosition + index].style.backgroundColor = ''
     })
   }
 
   //stop the tetromino
-  freeze(current) {
+  freeze() {
     //if the row below the tetromino contains a 'taken' div, change the divs where the tetromino lies to also be taken.
-    if(current.some(index => this.grid[currentPosition + index + width].classList.contains('taken'))) {
-      current.forEach(index => this.grid[currentPosition + index].classList.add('taken'))
+    if(this.current.some(index => this.grid[this.currentPosition + index + this.width].classList.contains('taken'))) {
+      this.current.forEach(index => this.grid[this.currentPosition + index].classList.add('taken'))
       //start a new tetromino falling
-      random = nextRandom
-      nextRandom = Math.floor(Math.random() * theTetrominos.length)
-      current = theTetrominos[random][currentRotation]
-      currentPosition = 4
-      draw(current)
-      addScore()
-      gameOver(current)
-      statsHelper()
+      this.random = this.nextRandom
+      this.nextRandom = Math.floor(Math.random() * this.theTetrominos.length)
+      this.current = this.theTetrominos[this.random][this.currentRotation]
+      this.currentPosition = 4
+      this.draw()
+      this.displayShape()
+      this.addScore()
+      this.gameOver()
+      this.statsHelper()
     }
   }
 
   //move the tetromino down
-  moveDown(current) {
-    undraw(current)
-    currentPosition += width
-    draw(current)
-    freeze(current)
+  moveDown() {
+    this.undraw()
+    this.currentPosition += this.width
+    this.draw()
+    this.freeze()
   }
 
   forceDown(current) {
-    while (!current.some(index => this.grid[currentPosition + index + width].classList.contains('taken'))) {
-      undraw(current)
-      currentPosition += width
-      draw(current)
+    while (!this.current.some(index => this.grid[this.currentPosition + index + this.width].classList.contains('taken'))) {
+      this.undraw(current)
+      this.currentPosition += this.width
+      this.draw(current)
     }
-    freeze(current)
+    this.freeze(current)
   }
 
-  rotate(current){
-    undraw(current)
-    pastRotation = currentRotation
-    currentRotation = (currentRotation + 1) % 4
-    current = theTetrominos[random][currentRotation]
-    if (current.some(index => this.grid[currentPosition + index].classList.contains('taken'))) {
+  rotate(){
+    this.undraw()
+    this.pastRotation = this.currentRotation
+    this.currentRotation = (this.currentRotation + 1) % 4
+    this.current = this.theTetrominos[this.random][this.currentRotation]
+    if (this.current.some(index => this.grid[this.currentPosition + index].classList.contains('taken'))) {
       console.log('cant rotate that!')
       //current = theTetrominos[random][pastRotation]
-      currentPosition -= width
+      this.currentPosition -= this.width
     }
-    draw(current)
-    freeze(current)
+    this.draw()
+    this.freeze()
   }
 
   //move the tetrominos left, unless it is at the edge or there is a blockage.
-  moveLeft(current){
-    undraw(current)
+  moveLeft(){
+    this.undraw()
     //if any of the indexes is touching the left edge (10 % width=(10) === 0)
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    const isAtLeftEdge = this.current.some(index => (this.currentPosition + index) % this.width === 0)
     //if okay, move it left
-    if (!isAtLeftEdge) currentPosition -= 1
+    if (!isAtLeftEdge) this.currentPosition -= 1
     //if colliding with any taken divs, move it back.
-    if (current.some(index => this.grid[currentPosition + index].classList.contains('taken'))) currentPosition += 1
-    draw(current)
+    if (this.current.some(index => this.grid[this.currentPosition + index].classList.contains('taken'))) this.currentPosition += 1
+    this.draw()
   }
 
   //move tetrominos right, same logic as moveLeft
-  moveRight(current){
-    undraw(current)
-    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
-    if (!isAtRightEdge) currentPosition += 1
-    if (current.some(index => this.grid[currentPosition + index].classList.contains('taken'))) currentPosition -= 1
-    draw(current)
+  moveRight(){
+    this.undraw()
+    const isAtRightEdge = this.current.some(index => (this.currentPosition + index) % this.width === this.width - 1)
+    if (!isAtRightEdge) this.currentPosition += 1
+    if (this.current.some(index => this.grid[this.currentPosition + index].classList.contains('taken'))) this.currentPosition -= 1
+    this.draw()
   }
 
   //add score and remove completed rows
   addScore() {
-    for (let i=0; i < 199; i += width) {
+    for (let i=0; i < 199; i += this.width) {
       const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
       if (row.every(index => this.grid[index].classList.contains('taken'))) {
-        score += 10
-        ScoreDisplay.innerHTML = score
+        this.score += 10
+        this.scoreText.innerHTML = this.score
         row.forEach(index => {
           this.grid[index].classList.remove('tetromino')
           this.grid[index].classList.remove('taken')
           this.grid[index].style.backgroundColor = ''
         })
-        const removed = this.grid.splice(i, width)
+        const removed = this.grid.splice(i, this.width)
         this.grid = removed.concat(this.grid)
-        this.grid.forEach(cell => grid.appendChild(cell))
+        this.grid.forEach(cell => this.parentGrid.appendChild(cell))
       }
     }
   }
 
   //game over
-  gameOver(current){
+  gameOver(){
     //if our block is inside any other blocks, the game ends. This happens when a block spawns into another one.
-    if (current.some(index => this.grid[currentPosition + index].classList.contains('taken'))){
-      ScoreDisplay.innerHTML = 'end'
-      clearInterval(timerId)
-      document.removeEventListener('keydown', control)
+    if (this.current.some(index => this.grid[this.currentPosition + index].classList.contains('taken'))){
+      this.scoreText.innerHTML = 'end'
+      clearInterval(this.timerId)
     }
   }
 
@@ -126,21 +214,22 @@ class Board {
   getHeight(){
     for (let i = 0; i < 209; i++){
       if (this.grid[i].classList.contains('taken')) {
-        heightText.innerHTML = (height - Math.floor(i / width)) //current level. height - (index found / width)
-        break 
+        const maxHeight = (this.height - Math.floor(i / this.width))
+        this.heightText.innerHTML = maxHeight
+        return maxHeight //current level. height - (index found / width)
       }
     }
   }
 
   getBumpiness(){
     //find height of first column,  add the abs() difference of height to the next column, continue
-    totalBumpiness = 0
+    let totalBumpiness = 0
     let newColumnHeight = 0
     let pastColumnHeight = -1
     for (let column = 0; column < 9; column++){
-      for (let i = column; i < height * width + width; i += width) { //i goes to 209 (height * width + width) in order to account for the '0' height floor
+      for (let i = column; i < this.height * this.width + this.width; i += this.width) { //i goes to 209 (height * width + width) in order to account for the '0' height floor
         if (this.grid[i].classList.contains('taken')) {
-          newColumnHeight = (height - Math.floor(i / width)) //current height formula
+          const newColumnHeight = (this.height - Math.floor(i / this.width)) //current height formula
           if (pastColumnHeight != -1){ //let it run once before doing math
             totalBumpiness += Math.abs(newColumnHeight - pastColumnHeight)
           }
@@ -149,26 +238,41 @@ class Board {
         }
       }
     }
-    bumpinessText.innerHTML = totalBumpiness
+    this.bumpinessText.innerHTML = totalBumpiness
+    return totalBumpiness
   }
 
   getHoles(){
     let numHoles = 0
     for (let column = 0; column < 9; column++){
       let blockHit = false
-      for (let i = column; i < height * width; i += width) {
+      for (let i = column; i < this.height * this.width; i += this.width) {
         //if there was a block higher up and this is an air pocket, that's a hole.
         if (blockHit && !this.grid[i].classList.contains('taken')) numHoles++
         else if (!blockHit && this.grid[i].classList.contains('taken')) blockHit = true
       }
     }
-    holesText.innerHTML = numHoles
+    this.holesText.innerHTML = numHoles
+    return numHoles
   }
 
   statsHelper(){
-    getHeight()
-    getBumpiness()
-    getHoles()
+    this.currentHeight = this.getHeight()
+    this.currentBump = this.getBumpiness()
+    this.currentHoles = this.getHoles()
+  }
+
+  //display the shape in the mini-grid
+  displayShape() {
+    //cleanse the whole minigrid
+    this.displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+      square.style.backgroundColor = ''
+    })
+    this.upNextTetrominoes[this.nextRandom].forEach(index => {
+      this.displaySquares[this.displayIndex + index].classList.add('tetromino')
+      this.displaySquares[this.displayIndex + index].style.backgroundColor = this.colors[this.nextRandom]
+    })
   }
 
 }
